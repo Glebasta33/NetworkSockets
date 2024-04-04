5cea2e3b65f66bc54425d0b4f5bfe49d06ed521b
Gleb Trusov <gleb.trusow@yandex.ru>
2024-04-04 09:28:33 +0300

    Добавил URLConnection для связи по HTTP-протоколу

---
+++ src/main/kotlin/HttpProtocolConnection.kt
@@ -0,0 +1,24 @@
+package com.github.gltrusov
+
+import java.net.URL
+import java.net.URLConnection
+import java.util.Scanner
+
+/**
+ * На чистых сокетах не получится подключиться к большинству серверов, тк они используют протоколы - более высокоуровневые надстройки над сокетами.
+ * URLConnection - это надстройка над сокетами, которая позволяет создать connection по http протоколу.
+ */
+fun main() {
+    val urlConnection: URLConnection = URL("http://www.google.com/").openConnection()
+
+    val scanner = Scanner(urlConnection.getInputStream())
+
+    while (scanner.hasNextLine()) {
+        println(scanner.nextLine())
+    }
+
+    urlConnection.headerFields.forEach { t, u ->
+        println("Header: $t | $u")
+    }
+
+}
